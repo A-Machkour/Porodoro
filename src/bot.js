791da6239a080b940068d63dc381eb1d5cd8f7e9
A-Machkour <abdoul.machkour@gmail.com>
2023-11-13 14:48:16 +0100

    Git cleaning

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,40 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
 
-const client = new Client({ intents:['Guilds', 'GuildMessages'] });
-const TOKEN= 'MTE0NjE1ODkyNjM5NjY3NDExOA.GGKk-2.upAnVVs9Bwdn3S--fMQ2Qno_gP10hjNLUm_F0E';
+dotenv.config();
+const client = new Client({ 
+    intents:[
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent,
+    ],
+ });
 
-client.login(TOKEN);
+
+client.on('ready', () => {
+    console.info(`Logged in as ${client.user.tag} !`);
+});
+
+client.on('messageCreate', (message) => {
+    const userInput = message.content.toLowerCase();
+    if (message.content === 'ping'&& !message.author.bot) {
+        message.reply('pong ! un peu de respect pour les bots !');
+    }
+    if (userInput == "!commands" || userInput == "!help") {
+        message.reply("this bot operates on the following commands: !help !commands !age !math");
+    }
+    if (userInput == "!age") {
+        console.log(message.author.createdAt);
+        message.reply("you are " + (new Date().getFullYear() - message.author.createdAt.getFullYear()) + " years old");
+    }
+    if (userInput == "!math") {
+        message.reply(" 59 * 836 * 852 = " + (59 * 836 * 852));
+    }
+
+
+});
+
+
+client.login(process.env.TOKEN);
